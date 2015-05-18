3908c33d69a8a43cb059b7360347917327a8f42e
Eoghan O'Brien <eoghan@eoghanobrien.com>
2015-05-18 17:37:07 +0100

    Allow non-core account links to appear in menu
    Currently, any community or local modules which try to append links to
    this menu are being hidden, this patch allows them to show below the
    configured menu. The starting position for the custom links is 150. All
    of the core links start with their position below 150 and can be
    configured to before or after any other link. They cannot as yet be
    turned off though.

--- app/code/community/Webguys/CustomerNavigation/Block/Account/Navigation.php
+++ app/code/community/Webguys/CustomerNavigation/Block/Account/Navigation.php
@@ -1,26 +1,115 @@
 <?php 
 
-class Webguys_CustomerNavigation_Block_Account_Navigation extends Mage_Customer_Block_Account_Navigation {
+class Webguys_CustomerNavigation_Block_Account_Navigation extends Mage_Customer_Block_Account_Navigation
+{
+    /** @var array */
+    protected $configuredLinks = array(
+        'account',
+        'account_edit',
+        'address_book',
+        'orders',
+        'billing_agreements',
+        'recurring_profiles',
+        'reviews',
+        'tags',
+        'newsletter',
+        'oauth_customer_tokens',
+        'downloadable_products',
+        'wishlist'
+    );
 
-	public function getLinks()
+    /**
+     * @inheritDoc
+     */
+    public function getLinks()
     {
-    	$pre_links 	  = $this->_links;
-    	$tmp_links    = array(); 
-		$this->_links = array();
-		
-		foreach ($pre_links as $_link) {
-			if( Mage::getStoreConfig( 'customernavigation/settings/show_' . $_link->getName() ) ) {
-				$tmp_links[ Mage::getStoreConfig( 'customernavigation/reorder/position_' . $_link->getName() ) ] = $_link;				
-			}            	
-		}
-		
-		ksort( $tmp_links );
-		
-		foreach ($tmp_links as $key=>$_link) {
-			if( Mage::getStoreConfig( 'customernavigation/settings/show_' . $_link->getName() ) ) {
-				$this->addLink($_link->getName(), $_link->getPath(), $_link->getLabel());				
-			}            	
-		}
-		return $this->_links;
+        $preLinks = $this->_links;
+        $tmpLinks = array();
+        $this->_links = array();
+
+        $order = 150;
+        foreach ($preLinks as $link) {
+            $isConfiguredLink = $this->isConfiguredLink($link);
+            // Add any core links configured to show in the module
+            if ($isConfiguredLink && $this->isConfiguredToShow($link)) {
+                $position = $this->getConfigPosition($link);
+                $tmpLinks[$position] = $link;
+            }
+            // Add any custom links added via layout XML
+            if (!$isConfiguredLink) {
+                $tmpLinks[$order] = $link;
+                $order += 10;
+            }
+        }
+
+        // Sort the new list
+        ksort($tmpLinks);
+
+        foreach ($tmpLinks as $position => $link) {
+            $isConfiguredLink = $this->isConfiguredLink($link);
+            // Add any core links configured to show in the module
+            if ($isConfiguredLink && $this->isConfiguredToShow($link)) {
+                $this->addLink($link->getName(), $link->getPath(), $link->getLabel());
+            }
+            // Add any custom links added via layout XML
+            if (!$isConfiguredLink) {
+                $this->addLink($link->getName(), $link->getPath(), $link->getLabel());
+            }
+        }
+
+        return $this->_links;
+    }
+
+    /**
+     * Determine if the given link is configured to be shown.
+     *
+     * @param Varien_Object $link
+     *
+     * @return mixed
+     */
+    protected function isConfiguredToShow(Varien_Object $link)
+    {
+        return Mage::getStoreConfig(
+            sprintf('customernavigation/settings/show_%s', $this->getFormattedName($link))
+        );
+    }
+
+    /**
+     * Get the configured position for the given link.
+     *
+     * @param Varien_Object $link
+     *
+     * @return mixed
+     */
+    protected function getConfigPosition(Varien_Object $link)
+    {
+        return Mage::getStoreConfig(
+            sprintf('customernavigation/reorder/position_%s', $this->getFormattedName($link))
+        );
+    }
+
+    /**
+     * Determine if the given link is a "configured" link.
+     *
+     * @param Varien_Object $link
+     *
+     * @return bool
+     */
+    protected function isConfiguredLink(Varien_Object $link)
+    {
+        $needle = $this->getFormattedName($link);
+        return (in_array($needle, $this->configuredLinks));
+    }
+
+    /**
+     * Get the formatted name e.g. my_link_name.
+     *
+     * @param Varien_Object $link
+     *
+     * @return mixed
+     */
+    protected function getFormattedName(Varien_Object $link)
+    {
+        return str_replace(' ', '_', strtolower($link->getName()));
     }
 }
